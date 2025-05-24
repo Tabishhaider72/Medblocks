c30e7763c59ac34baa891841ee7e73cb155bb485
SayedTabish72 <sayedtabish72@gmail.com>
2025-05-24 14:44:26 +0530

    feat: add dynamic dropdown data in departments.js

---
+++ src/utils/departments.js
@@ -0,0 +1,52 @@
+export const departments = [
+  {
+    name: "Cardiology",
+    symptoms: ["Chest Pain", "Shortness of Breath"],
+    doctors: ["Dr. Ahuja", "Dr. Mehra"]
+  },
+  {
+    name: "Neurology",
+    symptoms: ["Migraine", "Seizures"],
+    doctors: ["Dr. Kapoor", "Dr. Rathi"]
+  },
+  {
+    name: "Orthopedics",
+    symptoms: ["Fracture", "Joint Pain"],
+    doctors: ["Dr. abbas", "Dr. Sharma"]
+  },
+  {
+    name: "Pediatrics",
+    symptoms: ["Fever", "Cough"],
+    doctors: ["Dr. Malik", "Dr. Singh"]
+  },
+  {
+    name: "Dermatology",
+    symptoms: ["Rashes", "Itching"],
+    doctors: ["Dr. Sinha", "Dr. Rawat"]
+  },
+  {
+    name: "ENT",
+    symptoms: ["Ear Pain", "Sore Throat"],
+    doctors: ["Dr. Khanna", "Dr. Gill"]
+  },
+  {
+    name: "Gastroenterology",
+    symptoms: ["Acidity", "Stomach Pain"],
+    doctors: ["Dr. Jain", "Dr. Qureshi"]
+  },
+  {
+    name: "Ophthalmology",
+    symptoms: ["Blurred Vision", "Red Eyes"],
+    doctors: ["Dr. Ray", "Dr. Shetty"]
+  },
+  {
+    name: "Psychiatry",
+    symptoms: ["Anxiety", "Depression"],
+    doctors: ["Dr. Raza", "Dr. George"]
+  },
+  {
+    name: "General Medicine",
+    symptoms: ["Fever", "Weakness"],
+    doctors: ["Dr. Hussain", "Dr. Paul"]
+  }
+];
